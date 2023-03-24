6a1c4037c6c2242a0a9e8af0060008e33b359258
isabelle-nobile <isabelle.nobile@laplateforme.io>
2023-03-24 16:28:19 +0100

    minimax working

---
+++ TicTacToe/ia_minimax.py
@@ -0,0 +1,123 @@
+from constants import *
+
+
+class Minimax:
+    def minimax(self, board, depth, is_maximizing_player, current_player, ai_player):
+        """
+        Cette fonction implémente l'algorithme Minimax avec élagage Alpha-Bêta pour choisir le meilleur coup
+        à jouer pour l'IA.
+
+        """
+        # On vérifie si le jeu est terminé ou si on atteint la profondeur maximale
+        if depth < 0:
+            return -1234
+
+        if self.check_win(board, current_player, ai_player) != None:
+            return self.check_win(board, current_player, ai_player)
+
+        if self.check_tie(board):
+            return 0
+        
+        if is_maximizing_player:
+            player = ai_player
+            best_score = float('-inf')
+            for i in range(3):
+                for j in range(3):
+                    if board[i][j] == ' ':
+                        # On simule le coup
+                        board[i][j] = player
+                        # On récursive l'appel à la fonction minimax pour évaluer la valeur de ce coup
+                        score = self.minimax(board, depth - 1, not is_maximizing_player,current_player, ai_player)
+                        # On met à jour la meilleure valeur
+                        best_score = max(best_score, score)
+                        board[i][j] = ' '
+        else:
+            best_score = float('inf')
+            player = current_player
+            for i in range(3):
+                for j in range(3):
+                    if board[i][j] == ' ':
+                        # On simule le coup
+                        board[i][j] = player
+                        # On récursive l'appel à la fonction minimax pour évaluer la valeur de ce coup
+                        score = self.minimax(board, depth - 1, not is_maximizing_player,current_player, ai_player)
+
+                        # On met à jour la meilleure valeur
+
+                        best_score = min(best_score, score)
+                        board[i][j] = ' '
+        # On boucle à travers chaque case vide du plateau
+        return best_score
+
+    def get_best_move(self, board, current_player, ai_player):
+        """
+        Cette fonction détermine le meilleur coup à jouer pour l'IA en utilisant l'algorithme Minimax.
+        """
+        best_score = -12345678
+        best_move = None
+
+        # On boucle à travers chaque case vide du plateau
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    # On simule le coup
+
+                    board[i][j] = ai_player
+                    # On évalue la valeur de ce coup en appelant la fonction minimax
+                    score = self.minimax(
+                        board, 6, False, current_player, ai_player)
+                    # On met à jour le meilleur
+                    board[i][j] = ' '
+                    if score > best_score:
+                        print(score)
+                        best_score = score
+                        best_move = (i, j)
+
+        return best_move
+
+    def check_win(self, board, current_player, ai_player):
+        # Check rows
+        for row in range(ROWS):
+            if board[row][0]  == board[row][1] == board[row][2] and  board[row][0] ==ai_player:
+            if board[row][0] != EMPTY and board[row][0] == board[row][1] == board[row][2]:
+                if board[row][0] == ai_player:
+                    return 10
+                elif board[row][0] == current_player:
+                    return -10
+        # Check columns
+        for col in range(COLS):
+            if board[0][col] != EMPTY and board[0][col] == board[1][col] == board[2][col]:
+                if board[0][col] == ai_player:
+                    return 10
+                elif board[0][col] == current_player:
+                    return -10
+        # Check diagonals
+        if board[0][0] != EMPTY and board[0][0] == board[1][1] == board[2][2]:
+            if board[0][0] == ai_player:
+                return 10
+            elif board[0][0] == current_player:
+                return -10
+        if board[0][2] != EMPTY and board[0][2] == board[1][1] == board[2][0]:
+            if board[0][2] == ai_player:
+                return 10
+            elif board[0][2] == current_player:
+                return -10
+        # Check if the game is a tie
+        if self.check_tie(board):
+            return 0
+        # If the game is not over yet, return None
+        return None
+
+    # def check_tie(self, board, current_player,ai_player):
+    #     for row in range(ROWS):
+    #         for col in range(COLS):
+    #             if board[row][col] == EMPTY:
+    #                 return False
+    #     return not self.check_win(board, current_player,ai_player)
+
+    def check_tie(self, board):
+        for row in board:
+            for cell in row:
+                if cell == EMPTY:
+                    return False
+        return True
--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from constants import *
 from ia import ia
-from ia_d import Minimax
+from ia_minimax import Minimax
 from ia_monte import MonteCarlo
 
 
@@ -10,26 +10,43 @@ class TicTacToe:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Tic Tac Toe")
-        self.canvas = tk.Canvas(self.root, width=WIDTH, height=HEIGHT, bg=BG_COLOR)
+        self.canvas = tk.Canvas(self.root, width=WIDTH,
+                                height=HEIGHT, bg=BG_COLOR)
         self.canvas.pack()
         self.create_menu_bar()
         self.draw_lines()
-        self.canvas.bind("<Button-1>", self.click_handler)
         self.board = [[EMPTY for _ in range(COLS)] for _ in range(ROWS)]
+
+        self.canvas.bind("<Button-1>", self.click_handler)
         self.current_player = CROSS
         self.game_over = False
         self.ai_player = CIRCLE
         self.minimax = Minimax()
         self.monte_carlo = MonteCarlo(10000)
         self.difficulty = None
+        self.score_x = 0
+        self.score_o = 0
         self.root.iconbitmap('icon_tic_tac_toe.ico')
+
+    def draw_scoreboard(self):
+        self.canvas.create_text(
+            WIDTH // 2, HEIGHT + OFFSET,
+            text=f"Score X: {self.score_x}    Score O: {self.score_o}",
+            font=("Arial", 16))
+
+    def update_scoreboard(self, winner):
+        if winner == CROSS:
+            self.score_x += 1
+        elif winner == CIRCLE:
+            self.score_o += 1
+        else:
+            pass
 
     def create_menu_bar(self):
         menu_bar = tk.Menu(self.root)
         self.root.config(menu=menu_bar)
 
         menu_file = tk.Menu(menu_bar, tearoff=0)
-
         # Un menu déroulant dans le menu Difficulty.
         menu_recent = tk.Menu(menu_file, tearoff=0)
         menu_recent.add_command(label="Easy", command=self.play_easy)
@@ -41,7 +58,6 @@ class TicTacToe:
         menu_edit = tk.Menu(menu_bar, tearoff=0)
         menu_edit.add_command(label="PvP", command=self.start_pvp)
         menu_bar.add_cascade(label="Play vs Player", menu=menu_edit)
-
 
         menu_reset = tk.Menu(menu_bar, tearoff=0)
         menu_reset.add_command(label="New Game", command=self.reset_game)
@@ -73,8 +89,9 @@ class TicTacToe:
 
     def start_pvp(self):
         # self.ai_player = None
+        self.difficulty = None  # ajout de la difficulté choisie
         self.reset_game()
-        
+
     def reset_game(self):
         self.board = [[EMPTY for _ in range(COLS)] for _ in range(ROWS)]
         self.current_player = CROSS
@@ -82,15 +99,17 @@ class TicTacToe:
         self.canvas.delete("all")
         self.draw_lines()
         self.ia_difficulty = None
-
+        self.draw_scoreboard()
 
     def draw_lines(self):
         for i in range(1, COLS):
             x = i * WIDTH // COLS
-            self.canvas.create_line(x, 0, x, HEIGHT, width=LINE_WIDTH, fill=LINE_COLOR)
+            self.canvas.create_line(
+                x, 0, x, HEIGHT, width=LINE_WIDTH, fill=LINE_COLOR)
         for i in range(1, ROWS):
             y = i * HEIGHT // ROWS
-            self.canvas.create_line(0, y, WIDTH, y, width=LINE_WIDTH, fill=LINE_COLOR)
+            self.canvas.create_line(
+                0, y, WIDTH, y, width=LINE_WIDTH, fill=LINE_COLOR)
 
     def click_handler(self, event):
         if self.game_over:
@@ -100,6 +119,7 @@ class TicTacToe:
         if self.board[row][col] != EMPTY:
             return
         self.board[row][col] = self.current_player
+
         if self.current_player == CROSS:
             self.draw_cross(row, col)
         else:
@@ -116,28 +136,29 @@ class TicTacToe:
                 self.ai_move_difficult()
             elif self.difficulty == 'difficult' and self.current_player == self.ai_player:
                 self.ai_move_difficult()
-
-
+            self.draw_scoreboard()
 
     def draw_cross(self, row, col):
         x1 = col * WIDTH // COLS + OFFSET
         y1 = row * HEIGHT // ROWS + OFFSET
         x2 = (col + 1) * WIDTH // COLS - OFFSET
         y2 = (row + 1) * HEIGHT // ROWS - OFFSET
-        self.canvas.create_line(x1, y1, x2, y2, width=CROSS_WIDTH, fill=CROSS_COLOR)
+        self.canvas.create_line(
+            x1, y1, x2, y2, width=CROSS_WIDTH, fill=CROSS_COLOR)
         x1 = (col + 1) * WIDTH // COLS - OFFSET
         y1 = row * HEIGHT // ROWS + OFFSET
         x2 = col * WIDTH // COLS + OFFSET
         y2 = (row + 1) * HEIGHT // ROWS - OFFSET
-        self.canvas.create_line(x1, y1, x2, y2, width=CROSS_WIDTH, fill=CROSS_COLOR)
-
+        self.canvas.create_line(
+            x1, y1, x2, y2, width=CROSS_WIDTH, fill=CROSS_COLOR)
 
     def draw_circle(self, row, col):
         x1 = col * WIDTH // COLS + OFFSET
         y1 = row * HEIGHT // ROWS + OFFSET
         x2 = (col + 1) * WIDTH // COLS - OFFSET
         y2 = (row + 1) * HEIGHT // ROWS - OFFSET
-        self.canvas.create_oval(x1, y1, x2, y2, width=CIRCLE_WIDTH, outline=CIRCLE_COLOR)
+        self.canvas.create_oval(
+            x1, y1, x2, y2, width=CIRCLE_WIDTH, outline=CIRCLE_COLOR)
 
     def switch_player(self):
         if self.current_player == CROSS:
@@ -171,6 +192,7 @@ class TicTacToe:
     def show_win_message(self):
         messagebox.showinfo("Game Over", f"{self.current_player} wins!")
         self.game_over = True
+        self.update_scoreboard(self.current_player)
 
     def show_tie_message(self):
         messagebox.showinfo("Game Over", "Tie game!")
@@ -187,13 +209,15 @@ class TicTacToe:
                 self.show_tie_message()
             else:
                 self.switch_player()
+                print(self.current_player)
 
         else:
             print("Error: AI couldn't make a move")
 
     def ai_move_medium(self):
         # Determine the best move for AI
-        best_move = self.monte_carlo.get_best_move(self.board, self.current_player, self.ai_player)
+        best_move = self.monte_carlo.get_best_move(
+            self.board, self.current_player, self.ai_player)
 
         # Update the board and UI
         self.board[best_move[0]][best_move[1]] = self.ai_player
@@ -206,23 +230,23 @@ class TicTacToe:
             self.show_tie_message()
         else:
             self.switch_player()
-    
+
     def ai_move_difficult(self):
         # Determine the best move for AI
-        best_move = self.minimax.get_best_move(self.board, self.current_player, self.ai_player)
+        best_move = self.minimax.get_best_move(
+            self.board, CROSS, self.ai_player)
 
         # Update the board and UI
         self.board[best_move[0]][best_move[1]] = self.ai_player
         self.draw_circle(best_move[0], best_move[1])
 
-        # Check if the game is over
+    # Check if the game is over
         if self.check_win():
             self.show_win_message()
         elif self.check_tie():
             self.show_tie_message()
         else:
             self.switch_player()
-
 
 
 if __name__ == "__main__":
